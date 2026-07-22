3c4922441b38b79a65c5a1cdc9c4022d03e8ec45
Nando <fernandocalva15@gmail.com>
2026-07-22 17:26:55 -0600

    Error, settings: issue no issues, apps y models: agrege el modelo en apps, lo cual es incorrecto

--- issue/apps.py
+++ issue/apps.py
@@ -1,16 +1,5 @@
-from issue import models
+from django.apps import AppConfig
 
 
-class IssueConfig(models.Model):
-    id = models.AutoField(primary_key=True)
-    status = models.booleanField(default=False)
-    title = models.CharField(max_length=100)
-    steps_to_reproduce = models.TextField()
-    result_expected = models.TextField()
-    result_obtained = models.TextField()
-    priority = models.CharField(max_length=10, choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
-    severity = models.CharField(max_length=10, choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
-    affected_application = models.CharField(max_length=100)
-    affected_version = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class IssueConfig(AppConfig):
+    name = 'issue'
