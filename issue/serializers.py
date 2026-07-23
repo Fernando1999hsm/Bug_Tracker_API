54ed886c1b8093449daac672267107c700dde531
Nando <fernandocalva15@gmail.com>
2026-07-23 17:53:57 -0600

    Nuevo serializer para view de lista, que admin pueda ver la app Issue, migracion completada

--- issue/serializers.py
+++ issue/serializers.py
@@ -5,3 +5,9 @@ class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
         fields = '__all__'
+
+
+class IssueListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'title','status', 'severity', 'affected_application', 'created_at']
--- issue/views.py
+++ issue/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
 from .models import Issue
-from .serializers import IssueSerializer
+from .serializers import IssueSerializer, IssueListSerializer
 
 class IssueListView(generics.ListAPIView):
     queryset = Issue.objects.all()
-    serializer_class = IssueSerializer
+    serializer_class = IssueListSerializer
 
 class IssueDetailView(generics.RetrieveAPIView):
     queryset = Issue.objects.all()
