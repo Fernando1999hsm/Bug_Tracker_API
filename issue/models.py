1b3982b11b1fe86a52b9879c920d693686da9da2
Nando <fernandocalva15@gmail.com>
2026-07-30 22:29:19 -0600

    Nuevo modelo para las aplicaciones monitoreadas y un serializer para visualizar los datos

--- issue/models.py
+++ issue/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Application(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Issue(models.Model):
     id = models.AutoField(primary_key=True)
     status = models.BooleanField(default=False)
@@ -10,7 +18,10 @@ class Issue(models.Model):
     result_obtained = models.TextField()
     priority = models.CharField(max_length=10, choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
     severity = models.CharField(max_length=10, choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
-    affected_application = models.CharField(max_length=100)
+    application = models.ForeignKey(Application, on_delete=models.PROTECT, related_name='issues')
     affected_version = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- issue/serializers.py
+++ issue/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
-from .models import Issue
+from .models import Issue, Application
 
 class IssueSerializer(serializers.ModelSerializer):
+    application = serializers.StringRelatedField()
+
     class Meta:
         model = Issue
         fields = '__all__'
 
 
 class IssueListSerializer(serializers.ModelSerializer):
+    application = serializers.StringRelatedField()
+
     class Meta:
         model = Issue
-        fields = ['id', 'title','status', 'severity', 'affected_application', 'created_at']
+        fields = ['id', 'title', 'severity', 'status', 'application', 'created_at']
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ['id', 'code', 'name']
