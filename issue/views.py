42cad516123982561f9661fb3c706cd4a42c7410
Nando <fernandocalva15@gmail.com>
2026-08-11 18:29:41 -0600

    Clase ReadOnlyMixin agregada, solo acepta peticiones get

--- issue/views.py
+++ issue/views.py
@@ -1,17 +1,25 @@
 from django.db.models import Count, Q
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Issue, Application
 from .serializers import IssueSerializer, IssueListSerializer, ApplicationSerializer
 
-class IssueListView(generics.ListAPIView):
+class ReadOnlyMixin:
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return Response(
+            {'error': 'Solo se permiten peticiones GET.'},
+            status=status.HTTP_200_OK
+        )
+
+class IssueListView(ReadOnlyMixin, generics.ListAPIView):
     queryset = Issue.objects.all()
     serializer_class = IssueListSerializer
 
-class IssueDetailView(generics.RetrieveAPIView):
+class IssueDetailView(ReadOnlyMixin, generics.RetrieveAPIView):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
 
-class ApplicationListView(generics.ListAPIView):
+class ApplicationListView(ReadOnlyMixin, generics.ListAPIView):
     queryset = Application.objects.annotate(
         open_issues_count=Count('issues', filter=Q(issues__status=True))
     )
